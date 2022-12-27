acbb76e6782b6c44eff1bb015cc9129fe5f12503
jakiasultana2022 <rokeyasbd@gmail.com>
2022-12-27 20:31:40 +0600

    footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import logo from '../../../assets/logo.png'
 const Footer = () => {
     return (
         <div>
-            
+           <footer className="footer footer-center p-10 bg-gradient-to-r from-cyan-500 to-blue-300 text-white rounded mt-96 ">
+           <div >
+     <img style={{height:'100px'}} src={logo} alt="logo" />
+     <div className="grid grid-flow-col gap-4">
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+
+   
+  </div>
+  <div>
+    <p>Copyright © 2022 - All right reserved by Talkline</p>
+  </div> 
+    </div>
+  
+  <div>
+  
+  </div> 
+  
+</footer> 
         </div>
     );
 };
